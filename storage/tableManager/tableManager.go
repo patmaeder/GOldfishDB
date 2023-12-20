92d5ec170a682169713a5a083cf2e5b346cd4d45
Patrick Mäder <padi.maeder@gmail.com>
2023-12-21 00:23:24 +0100

    Fix tableManager.go

--- storage/tableManager/tableManager.go
+++ storage/tableManager/tableManager.go
@@ -2,20 +2,23 @@ package tableManager
 
 import (
 	"DBMS/fs"
-	"DBMS/storage/databaseManager"
 	"errors"
 	"os"
 	"regexp"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
+var dataDirectory = os.Getenv("DATA_DIR")
+
 func CreateTable(dbName string, tableName string) error {
-	if _, err := os.Stat(databaseManager.dataDirectory + "/" + dbName); err != nil {
+	if _, err := os.Stat(dataDirectory + "/" + dbName); err != nil {
 		if os.IsNotExist(err) {
 			return errors.New("database does not exist")
 		}
 	}
 
-	if _, err := os.Stat(databaseManager.dataDirectory + "/" + dbName + "/" + tableName + ".frm"); err == nil {
+	if _, err := os.Stat(dataDirectory + "/" + dbName + "/" + tableName + ".frm"); err == nil {
 		return errors.New("table with the given name already exists")
 	}
 
@@ -24,11 +27,11 @@ func CreateTable(dbName string, tableName string) error {
 		return errors.New("invalid table name. Must start with a letter or an underscore and be between 1 and 64 characters long")
 	}
 
-	if err := fs.Create(databaseManager.dataDirectory + "/" + dbName + "/" + tableName + ".frm"); err != nil {
+	if err := fs.Create(dataDirectory + "/" + dbName + "/" + tableName + ".frm"); err != nil {
 		return err
 	}
 
-	if err := fs.Create(databaseManager.dataDirectory + "/" + dbName + "/" + tableName + ".idb"); err != nil {
+	if err := fs.Create(dataDirectory + "/" + dbName + "/" + tableName + ".idb"); err != nil {
 		return err
 	}
 
