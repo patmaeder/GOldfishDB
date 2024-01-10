9ec09b558643d12ad8f9d424fda6d2a7318e26f7
Patrick Mäder <padi.maeder@gmail.com>
2024-01-10 16:53:15 +0100

    Replace any with value.Value in row struct

--- storage/row.go
+++ storage/row.go
@@ -1,18 +1,12 @@
 package storage
 
+import "DBMS/storage/value"
+
 type Row struct {
-	Entries map[[128]byte]any
+	Entries map[[128]byte]value.Value
 }
 
-func NewRow(entries map[[128]byte]any) Row {
-	/*convertedEntries := make(map[[128]byte]any)
-
-	for columnName, value := range entries {
-		columnNameBytes := []byte(columnName)
-		columnNameByteArray := append(columnNameBytes, make([]byte, 128-len(columnNameBytes))...)
-		convertedEntries[[128]byte(columnNameByteArray)] = value
-	}*/
-
+func NewRow(entries map[[128]byte]value.Value) Row {
 	return Row{
 		Entries: entries,
 	}
